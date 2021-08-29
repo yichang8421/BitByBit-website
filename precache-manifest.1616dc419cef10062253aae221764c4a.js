b4d2b4ee1c2554ba540aaedb8e3e511024ce52a3
andy8421 <yichang8421@qq.com>
2021-08-29 20:15:10 +0800

    deploy

---
+++ precache-manifest.1616dc419cef10062253aae221764c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895c8260d195e346a083",
+    "url": "/BitByBit-website/css/app.c02976d9.css"
+  },
+  {
+    "revision": "04bc65b109d3e912ff8d76d20ef939d6",
+    "url": "/BitByBit-website/index.html"
+  },
+  {
+    "revision": "895c8260d195e346a083",
+    "url": "/BitByBit-website/js/app.87408fa8.js"
+  },
+  {
+    "revision": "a17836e128b16f8a683a",
+    "url": "/BitByBit-website/js/chunk-vendors.89ec886e.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/BitByBit-website/manifest.json"
+  },
+  {
+    "revision": "e79b258847b115660c5c361accf28609",
+    "url": "/BitByBit-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BitByBit-website/robots.txt"
+  }
+]);
